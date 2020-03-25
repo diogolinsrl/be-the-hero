94169fc3e2a221cf4627c0e9b28fbea203bf1bf2
Diogo Lins <diogo.lins@gmail.com>
2020-03-25 09:45:25 -0300

    Backend

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors()); // em produção, precisaria configura o origin como parametro do cors
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
